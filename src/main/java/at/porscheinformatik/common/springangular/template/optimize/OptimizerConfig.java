416341f61ff857c978e3683c470fde6ffaf4ec86
furti <daniel.furtlehner@gmx.net>
2014-02-06 19:33:15 +0100

    Todos added

--- src/main/java/at/porscheinformatik/common/springangular/template/optimize/OptimizerConfig.java
+++ src/main/java/at/porscheinformatik/common/springangular/template/optimize/OptimizerConfig.java
@@ -9,8 +9,14 @@ public interface OptimizerConfig
 {
 	// TODO: hier sollte noch die möglichkeit rein das man die optimizer
 	// sortieren kann. Sowas wie "before:anotheroptimizer"
+	/*
+	 * TODO: denke wir brauchen hier keinen Namen. So wie SpringSecurity das
+	 * macht mit before <Klasse des Optimizers> müsste es funktionieren.
+	 */
 	void addOptimizer(ResourceType type, String name,
 			ResourcePreProcessor processor);
+
+	// TODO: möglichkeit Optimizer zu entfernen.
 
 	List<ResourcePreProcessor> getProcessors(ResourceType type);
 }

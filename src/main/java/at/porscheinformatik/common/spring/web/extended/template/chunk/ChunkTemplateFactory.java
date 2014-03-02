4424d6592c728859d9999bb191d6ef8863d518fa
furti <daniel.furtlehner@gmx.net>
2014-03-02 09:32:57 +0100

    added chunk-template engine

---
+++ src/main/java/at/porscheinformatik/common/spring/web/extended/template/chunk/ChunkTemplateFactory.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright 2014 Daniel Furtlehner
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package at.porscheinformatik.common.spring.web.extended.template.chunk;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.util.CollectionUtils;
+
+import at.porscheinformatik.common.spring.web.extended.expression.ExpressionHandler;
+import at.porscheinformatik.common.spring.web.extended.expression.ExpressionHandlers;
+import at.porscheinformatik.common.spring.web.extended.io.ResourceType;
+import at.porscheinformatik.common.spring.web.extended.template.Template;
+import at.porscheinformatik.common.spring.web.extended.template.TemplateFactory;
+
+import com.x5.template.Theme;
+
+public class ChunkTemplateFactory implements TemplateFactory
+{
+	private ExpressionHandlers expressionHandlers;
+	private Theme theme;
+
+	@Override
+	public Template createTemplate(Resource resource, String templateName,
+			ResourceType type, boolean optimized)
+	{
+		try
+		{
+			ChunkTemplate template = new ChunkTemplate(type, templateName,
+					optimized, resource, theme);
+			template.refresh();
+
+			return template;
+		} catch (IOException ex)
+		{
+			throw new RuntimeException(
+					"Error creating ChunkTemplate for template " + templateName,
+					ex);
+		}
+	}
+
+	@PostConstruct
+	public void buildTheme()
+	{
+		theme = new Theme();
+
+		Collection<String> expressionHandlerNames = expressionHandlers
+				.getHandlerNames();
+
+		if (CollectionUtils.isEmpty(expressionHandlerNames))
+		{
+			return;
+		}
+
+		for (String handlerName : expressionHandlerNames)
+		{
+			ExpressionHandler handler = expressionHandlers
+					.getHandler(handlerName);
+
+			theme.addProtocol(new ExpressionHandlerContentSource(handlerName,
+					handler));
+		}
+	}
+
+	@Autowired
+	public void setExpressionHandlers(ExpressionHandlers expressionHandlers)
+	{
+		this.expressionHandlers = expressionHandlers;
+	}
+}

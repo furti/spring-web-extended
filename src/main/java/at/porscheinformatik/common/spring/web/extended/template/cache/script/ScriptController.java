d6bb0a9fff86bbcd319447c69a143815ee07797c
furti <daniel.furtlehner@gmx.net>
2014-03-30 09:54:50 +0200

    added caching for styles and scripts

--- src/main/java/at/porscheinformatik/common/spring/web/extended/template/cache/script/ScriptController.java
+++ src/main/java/at/porscheinformatik/common/spring/web/extended/template/cache/script/ScriptController.java
@@ -1,4 +1,21 @@
+/**
+ * Copyright 2014 Daniel Furtlehner
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package at.porscheinformatik.common.spring.web.extended.template.cache.script;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,21 +24,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import at.porscheinformatik.common.spring.web.extended.template.ResourceControllerBase;
 import at.porscheinformatik.common.spring.web.extended.util.ResourceNotFoundException;
 
 @Controller
 @RequestMapping(value = "/**/script")
-public class ScriptController
+public class ScriptController extends ResourceControllerBase
 {
 
-	//TODO: caching der scripts
 	private ScriptStacks stacks;
 
 	@RequestMapping(value = "/single/{stackId}/{scriptName}", method = RequestMethod.GET, produces = "text/javascript")
 	@ResponseBody
 	public String handleStylesheet(
 			@PathVariable("stackId") String stackId,
-			@PathVariable("scriptName") String scriptName)
+			@PathVariable("scriptName") String scriptName,
+			HttpServletResponse response)
 	{
 		if (stacks == null || !stacks.hasStack(stackId))
 		{
@@ -35,12 +53,15 @@ public class ScriptController
 			throw new ResourceNotFoundException();
 		}
 
+		handleCaching(response);
+
 		return stack.renderTemplate(scriptName);
 	}
 
 	@RequestMapping(value = "/stack/{stackId}", method = RequestMethod.GET, produces = "text/javascript")
 	@ResponseBody
-	public String handleStack(@PathVariable("stackId") String stackId)
+	public String handleStack(@PathVariable("stackId") String stackId,
+			HttpServletResponse response)
 	{
 		if (stacks == null || !stacks.hasStack(stackId))
 		{
@@ -48,6 +69,8 @@ public class ScriptController
 		}
 
 		ScriptStack stack = stacks.get(stackId);
+
+		handleCaching(response);
 
 		return stack.renderAll();
 	}
--- src/main/java/at/porscheinformatik/common/spring/web/extended/template/cache/style/StyleController.java
+++ src/main/java/at/porscheinformatik/common/spring/web/extended/template/cache/style/StyleController.java
@@ -1,4 +1,21 @@
+/**
+ * Copyright 2014 Daniel Furtlehner
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package at.porscheinformatik.common.spring.web.extended.template.cache.style;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,21 +24,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import at.porscheinformatik.common.spring.web.extended.template.ResourceControllerBase;
 import at.porscheinformatik.common.spring.web.extended.util.ResourceNotFoundException;
 
 @Controller
 @RequestMapping(value = "/**/style")
-public class StyleController
+public class StyleController extends ResourceControllerBase
 {
-
-	// TODO: Caching der styles
 	private StyleStacks stacks;
 
 	@RequestMapping(value = "/single/{stackId}/{styleName}", method = RequestMethod.GET, produces = "text/css")
 	@ResponseBody
 	public String handleStylesheet(
 			@PathVariable("stackId") String stackId,
-			@PathVariable("styleName") String styleName)
+			@PathVariable("styleName") String styleName,
+			HttpServletResponse response)
 	{
 		if (stacks == null || !stacks.hasStack(stackId))
 		{
@@ -35,12 +52,15 @@ public class StyleController
 			throw new ResourceNotFoundException();
 		}
 
+		handleCaching(response);
+
 		return stack.renderTemplate(styleName);
 	}
 
 	@RequestMapping(value = "/stack/{stackId}", method = RequestMethod.GET, produces = "text/css")
 	@ResponseBody
-	public String handleStack(@PathVariable("stackId") String stackId)
+	public String handleStack(@PathVariable("stackId") String stackId,
+			HttpServletResponse response)
 	{
 		if (stacks == null || !stacks.hasStack(stackId))
 		{
@@ -48,6 +68,8 @@ public class StyleController
 		}
 
 		StyleStack stack = stacks.get(stackId);
+
+		handleCaching(response);
 
 		// TODO: is it a good idea to combine all styles in the stack? IE css
 		// file size limit?

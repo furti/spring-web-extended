fcbe79d1e94636997aa6d8b825e60104d6dc5f11
furti <daniel.furtlehner@gmx.net>
2014-06-03 15:40:22 +0200

    template got cached on the server when nocache was set.

--- src/main/java/at/porscheinformatik/common/spring/web/extended/template/cache/StackBase.java
+++ src/main/java/at/porscheinformatik/common/spring/web/extended/template/cache/StackBase.java
@@ -15,150 +15,142 @@ import at.porscheinformatik.common.spring.web.extended.util.SpringWebExtendedUti
 
 public abstract class StackBase extends AbstractTemplateCache
 {
-	private ResourceType resourceType;
-	private boolean noCaching;
-	protected Logger logger = LoggerFactory.getLogger(getClass());
+    private ResourceType resourceType;
+    protected Logger logger = LoggerFactory.getLogger(getClass());
 
-	public StackBase(ResourceType resourceType, String stackName,
-			boolean noCaching)
-	{
-		super(stackName);
-		this.resourceType = resourceType;
-		this.noCaching = noCaching;
-		setupLastRefresh();
-	}
+    public StackBase(ResourceType resourceType, String stackName,
+        boolean noCaching)
+    {
+        super(stackName, noCaching);
+        this.resourceType = resourceType;
+        setupLastRefresh();
+    }
 
-	public void addResource(String name, StackEntry entry) throws IOException
-	{
-		if (entry.isScanLocation())
-		{
-			scanResources(entry.getLocation(), entry.isSkipProcessing());
-		}
-		else
-		{
-			addResource(name, entry.getLocation(), false,
-					entry.isSkipProcessing());
-			addResource(name, entry.getMinifiedLocation(), true,
-					entry.isSkipProcessing());
-		}
-	}
+    public void addResource(String name, StackEntry entry) throws IOException
+    {
+        if (entry.isScanLocation())
+        {
+            scanResources(entry.getLocation(), entry.isSkipProcessing());
+        }
+        else
+        {
+            addResource(name, entry.getLocation(), false,
+                entry.isSkipProcessing());
+            addResource(name, entry.getMinifiedLocation(), true,
+                entry.isSkipProcessing());
+        }
+    }
 
-	/**
-	 * Scan all resources in the location and add them to the stack
-	 * 
-	 * @param location
-	 * @throws IOException
-	 */
-	private void scanResources(String location, boolean skipProcessing)
-			throws IOException
-	{
-		Map<String, Resource> resources = scanners.scanResources(location);
+    /**
+     * Scan all resources in the location and add them to the stack
+     * 
+     * @param location
+     * @throws IOException
+     */
+    private void scanResources(String location, boolean skipProcessing)
+        throws IOException
+    {
+        Map<String, Resource> resources = scanners.scanResources(location);
 
-		if (resources == null)
-		{
-			logger.warn("No resources found in location " + location);
-			return;
-		}
+        if (resources == null)
+        {
+            logger.warn("No resources found in location " + location);
+            return;
+        }
 
-		for (Entry<String, Resource> entry : resources.entrySet())
-		{
-			addTemplate(entry.getKey().toLowerCase(), location,
-					entry.getValue(),
-					resourceType, false, skipProcessing);
-		}
-	}
+        for (Entry<String, Resource> entry : resources.entrySet())
+        {
+            addTemplate(entry.getKey().toLowerCase(), location,
+                entry.getValue(),
+                resourceType, false, skipProcessing);
+        }
+    }
 
-	private void addResource(String name, String location,
-			boolean optimizedResource, boolean skipProcessing)
-			throws IOException
-	{
-		if (location == null)
-		{
-			return;
-		}
+    private void addResource(String name, String location,
+        boolean optimizedResource, boolean skipProcessing)
+        throws IOException
+    {
+        if (location == null)
+        {
+            return;
+        }
 
-		String[] prefixAndPath = SpringWebExtendedUtils
-				.parseExpression(location);
-		String[] pathAndFile = ResourceUtils.pathAndFile(prefixAndPath[1]);
+        String[] prefixAndPath = SpringWebExtendedUtils
+            .parseExpression(location);
+        String[] pathAndFile = ResourceUtils.pathAndFile(prefixAndPath[1]);
 
-		if (pathAndFile == null)
-		{
-			return;
-		}
+        if (pathAndFile == null)
+        {
+            return;
+        }
 
-		String locationToUse = null;
+        String locationToUse = null;
 
-		if (StringUtils.hasText(prefixAndPath[0]))
-		{
-			locationToUse = prefixAndPath[0] + ":";
-		}
-		else
-		{
-			locationToUse = "";
-		}
+        if (StringUtils.hasText(prefixAndPath[0]))
+        {
+            locationToUse = prefixAndPath[0] + ":";
+        }
+        else
+        {
+            locationToUse = "";
+        }
 
-		locationToUse += pathAndFile[0];
+        locationToUse += pathAndFile[0];
 
-		Map<String, Resource> resources = scanners.scanResources(
-				locationToUse, pathAndFile[1], false);
+        Map<String, Resource> resources = scanners.scanResources(
+            locationToUse, pathAndFile[1], false);
 
-		/*
-		 * Spring does not log the message right if we only throw a
-		 * illegalargumentexception. So we log it manually here
-		 */
-		if (resources == null)
-		{
-			String message = "No Resources for name " + name
-					+ " and location " + location + " found";
+        /*
+         * Spring does not log the message right if we only throw a
+         * illegalargumentexception. So we log it manually here
+         */
+        if (resources == null)
+        {
+            String message = "No Resources for name " + name
+                + " and location " + location + " found";
 
-			IllegalArgumentException ex = new IllegalArgumentException(message);
-			logger.error(message, ex);
+            IllegalArgumentException ex = new IllegalArgumentException(message);
+            logger.error(message, ex);
 
-			throw ex;
-		}
+            throw ex;
+        }
 
-		for (Entry<String, Resource> entry : resources.entrySet())
-		{
-			addTemplate(prepareName(name, entry.getKey()), location,
-					entry.getValue(),
-					resourceType, optimizedResource, skipProcessing);
-		}
-	}
+        for (Entry<String, Resource> entry : resources.entrySet())
+        {
+            addTemplate(prepareName(name, entry.getKey()), location,
+                entry.getValue(),
+                resourceType, optimizedResource, skipProcessing);
+        }
+    }
 
-	/**
-	 * Add the locale to the name
-	 * 
-	 * @param name
-	 * @param key
-	 * @return
-	 */
-	private String prepareName(String styleName, String resourceName)
-	{
-		String[] styleNameAndEnding = ResourceUtils.getNameAndEnding(styleName);
-		String[] resourceNameAndEnding = ResourceUtils
-				.getNameAndEnding(resourceName);
+    /**
+     * Add the locale to the name
+     * 
+     * @param name
+     * @param key
+     * @return
+     */
+    private String prepareName(String styleName, String resourceName)
+    {
+        String[] styleNameAndEnding = ResourceUtils.getNameAndEnding(styleName);
+        String[] resourceNameAndEnding = ResourceUtils
+            .getNameAndEnding(resourceName);
 
-		String locale = ResourceUtils
-				.getLocaleFromName(resourceNameAndEnding[0]);
+        String locale = ResourceUtils
+            .getLocaleFromName(resourceNameAndEnding[0]);
 
-		StringBuilder name = new StringBuilder(styleNameAndEnding[0]);
+        StringBuilder name = new StringBuilder(styleNameAndEnding[0]);
 
-		if (locale != null)
-		{
-			name.append("_").append(locale);
-		}
+        if (locale != null)
+        {
+            name.append("_").append(locale);
+        }
 
-		if (styleNameAndEnding[1] != null)
-		{
-			name.append(styleNameAndEnding[1]);
-		}
+        if (styleNameAndEnding[1] != null)
+        {
+            name.append(styleNameAndEnding[1]);
+        }
 
-		return name.toString();
-	}
-
-	public boolean isNoCaching()
-	{
-		return noCaching;
-	}
-
+        return name.toString();
+    }
 }
